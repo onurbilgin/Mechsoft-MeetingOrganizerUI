10597b5b8740ff9e8d82c48f7d5396d2d5b1841f
Onur Can Bilgin <onurbilgin73@gmail.com>
2022-08-23 01:35:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {Home} from './Home';
+import {Meeting} from './Meeting';
+import {BrowserRouter, Route, Routes,NavLink} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="App container">
+     
+      <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+        <ul className="navbar-nav">
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/home">
+              Toplantılarım
+            </NavLink>
+          </li>
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-success" to="/meeting">
+              Toplantı Düzenle
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+     
+
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/meeting' element={<Meeting/>}/>
+        </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
